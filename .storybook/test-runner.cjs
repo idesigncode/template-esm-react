e75160edd93c538ea16bdfec67c612fd05573103
Matthew Nuthall <15828266+idesigncode@users.noreply.github.com>
2023-05-04 06:41:46 +1000

    Added Image Snapshot testing

--- .storybook/test-runner.cjs
+++ .storybook/test-runner.cjs
@@ -1,8 +1,27 @@
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
+
+const customSnapshotsDir = `${process.cwd()}/__snapshots__`;
+
 module.exports = {
-  async postRender(page) {
+  setup() {
+    expect.extend({ toMatchImageSnapshot });
+  },
+  async postRender(page, context) {
     // ? Reference: https://github.com/storybookjs/test-runner#dom-snapshot-recipe
     const elementHandler = await page.$('#storybook-root');
     const innerHTML = await elementHandler.innerHTML();
     expect(innerHTML).toMatchSnapshot();
+
+    // ? Reference: https://github.com/storybookjs/test-runner#image-snapshot-recipe
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot({
+      customDiffDir: `${customSnapshotsDir}/failed/diff`,
+      customReceivedDir: `${customSnapshotsDir}/failed`,
+      customSnapshotsDir,
+      customSnapshotIdentifier: context.id,
+      failureThreshold: 0.015,
+      failureThresholdType: 'percent',
+      storeReceivedOnFailure: true,
+    });
   },
 };
